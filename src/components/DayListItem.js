8cf76207f772be171b20067e526d6a33a12728d1
Michael <michaelkrishna@hotmail.com>
2020-12-07 15:54:04 -0800

    Format spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,7 +2,11 @@ import React from "react";
 import classNames from "classnames";
 import "components/DayListItem.scss";
 
-
+const formatSpots = (spots) => {
+  if (spots === 0) return `no spots remaining`;
+  if (spots === 1) return `1 spot remaining`;
+  return `${spots} spots remaining`;
+}
 
 export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
@@ -12,7 +16,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
